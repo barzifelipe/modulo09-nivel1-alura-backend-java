42505e7b98d7ec7ec336ee7054471a041e30366e
barzifelipe <barzilaifelipe@gmail.com>
2025-12-26 12:33:08 -0300

    Add; exercicio02

---
+++ exercicio02/src/Livro.java
@@ -0,0 +1,9 @@
+public class Livro {
+    public String titulo;
+    public String autor;
+    public int paginas;
+
+    public void exibirDados(){
+        System.out.println(titulo+ " de "+autor+" com "+ paginas+" páginas");
+    }
+}
---
+++ exercicio02/src/Main02.java
@@ -0,0 +1,10 @@
+public class Main02 {
+    public static void main(String[] args){
+        Livro livro1 = new Livro();
+        livro1.titulo = "O Guia do Mochileiro das Galáxias";
+        livro1.autor = "Douglas Adams";
+        livro1.paginas = 208;
+
+        livro1.exibirDados();
+    }
+}
